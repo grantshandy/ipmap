0847655376b7b55e9212f1f0e653d52482fa7f81
Aditya Suresh <ethanaditya@gmail.com>
2021-10-10 21:36:04 -0600

    changed the way ip_index is handled into struct

--- src/ip.rs
+++ src/ip.rs
@@ -1,5 +1,7 @@
 use std::net::IpAddr;
 use std::sync::RwLock;
+use std::collections::HashSet; 
+use std::hash::{Hash, Hasher};
 
 use etherparse::{InternetSlice, SlicedPacket};
 use ipgeolocate::{Locator, Service};
@@ -8,12 +10,35 @@ use pcap::Device;
 
 pub static IP_JSON_DOCUMENT: Lazy<RwLock<String>> = Lazy::new(|| RwLock::new(String::new()));
 
+#[derive(Debug)]
+pub struct Node {
+    ip: Vec<IpAddr>,
+    latitude: f64, 
+    longitude: f64,
+}
+impl Node {
+    fn new(ip: Vec<IpAddr>, latitude: f64, longitude: f64) -> Self {
+        Node {
+            ip,
+            latitude,
+            longitude,
+        }
+    }
+}
+impl PartialEq for Node {
+    fn eq (&self, other: &Self) -> bool {
+        self.latitude == other.latitude && self.longitude == other.longitude
+    }
+}
+impl Eq for Node {}
+
 pub async fn manage_ip(cap: Device) {
     let mut cap = cap.open().unwrap();
+    let mut nodes: Vec<Node> = Vec::new();
 
-    let mut ip_index: Vec<IpAddr> = Vec::new();
-    let mut lat_index: Vec<f64> = Vec::new();
-    let mut lon_index: Vec<f64> = Vec::new();
+    // let mut ip_index: Vec<IpAddr> = Vec::new();
+    // let mut lat_index: Vec<f64> = Vec::new();
+    // let mut lon_index: Vec<f64> = Vec::new();
 
     // I have to do this in a loop because cap.next() hangs or something on mac OSX and windows for some reason.
     // Not great for performance but it had to be done.
@@ -38,7 +63,7 @@ pub async fn manage_ip(cap: Device) {
                     continue;
                 }
 
-                try_add_ip(ip, &mut ip_index, &mut lat_index, &mut lon_index).await;
+                try_add_ip(ip, &mut nodes).await;
             }
         }
     }
@@ -46,49 +71,38 @@ pub async fn manage_ip(cap: Device) {
 
 async fn try_add_ip(
     ip: IpAddr,
-    ip_index: &mut Vec<IpAddr>,
-    lat_index: &mut Vec<f64>,
-    lon_index: &mut Vec<f64>,
+    nodes: &mut Vec<Node>,
 ) {
-    if !ip_index.contains(&ip) {
-        ip_index.push(ip);
 
-        let (lat, lon, city) = match Locator::get_ipaddr(ip, Service::IpApi).await {
+    let (lat, lon, city) = match Locator::get_ipaddr(ip, Service::IpApi).await {
+        Ok(data) => (
+            data.latitude.parse::<f64>().unwrap(),
+            data.longitude.parse::<f64>().unwrap(),
+            data.city,
+        ),
+        Err(_error) => match Locator::get_ipaddr(ip, Service::IpWhois).await {
             Ok(data) => (
                 data.latitude.parse::<f64>().unwrap(),
                 data.longitude.parse::<f64>().unwrap(),
                 data.city,
             ),
-            Err(_error) => match Locator::get_ipaddr(ip, Service::IpWhois).await {
-                Ok(data) => (
-                    data.latitude.parse::<f64>().unwrap(),
-                    data.longitude.parse::<f64>().unwrap(),
-                    data.city,
-                ),
-                Err(_error) => (0.0, 0.0, String::new()),
-            },
-        };
+            Err(_error) => return,
+        },
+    };
 
-        if (!lat_index.contains(&lat) || !lon_index.contains(&lon)) && (lat != 0.0 && lon != 0.0) {
-            lat_index.push(lat);
-            lon_index.push(lon);
-
+    let new_node: Node = Node::new(vec![ip], lat, lon); 
+    match nodes.iter().position(|x| *x == new_node ) {
+        Some(i) => {
+            nodes[i].ip.push(ip);
+            println!("{}, {} has already been added", lat, lon);
+        },
+        None => {
+            nodes.push(new_node);
             // I do JSON here manually cuz we only need to serialize basic values.
             // We used to call serde_json on each new IP, that was a nightmare, this is much nicer :)
-            IP_JSON_DOCUMENT.write().unwrap().push_str(&format!(
-                r#"{{"ip":"{}","lat":"{}","lon":"{}","city":"{}",}},"#,
-                ip, lat, lon, city
-            ));
+            IP_JSON_DOCUMENT.write().unwrap().push_str(&format!(r#"{{"ip":"{}","lat":"{}","lon":"{}","city":"{}",}},"#,ip, lat, lon, city));
 
             println!("{} - ({}, {})", ip, lat, lon);
-        }
+        },
     }
-}
-
-#[derive(Clone)]
-pub struct IpAddress {
-    pub ip: IpAddr,
-    pub lat: f64,
-    pub lon: f64,
-    pub city: String,
 }
