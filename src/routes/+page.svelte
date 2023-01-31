f31b242c58a240b056512294ba79beecd8a29e94
grantshandy <granthandy@proton.me>
2023-01-31 15:07:13 -0700

    start work with sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import Map from "ol/Map.js";
+  import OSM from "ol/source/OSM.js";
+  import TileLayer from "ol/layer/Tile.js";
+  import View from "ol/View.js";
+
+  let map;
+
+  onMount(() => {
+    map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+  });
+</script>
+
+<div class="w-screen h-screen bg-blue-500">
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1>About Ipmap</h1>
