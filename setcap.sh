427bf89ec0dcc84e589eef7b1ed31cc9629403ab
grantshandy <granthandy@proton.me>
2023-02-02 15:27:42 -0700

    nth refactor

--- setcap.sh
+++ setcap.sh
@@ -6,7 +6,7 @@ echo "Running custom setcap.sh script."
 
 if [[ ! "$TAURI_PLATFORM" ]]; then
   cargo $1 --no-default-features
-  sudo setcap cap_net_raw,cap_net_admin=eip target/debug/ipmap
+  sudo setcap cap_net_raw,cap_net_admin=eip ../target/debug/ipmap
 else
   sudo setcap cap_net_raw,cap_net_admin=eip target/release/ipmap
 fi
