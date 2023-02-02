31433b18b576780946a36fc77e66bcef1657cd58
grantshandy <granthandy@proton.me>
2023-02-02 09:12:17 -0700

    fix weird update issue

--- setcap.sh
+++ setcap.sh
@@ -5,7 +5,7 @@ set -e
 echo "Running custom setcap.sh script."
 
 if [[ ! "$TAURI_PLATFORM" ]]; then
-  cargo $1
+  cargo $1 --no-default-features
   sudo setcap cap_net_raw,cap_net_admin=eip target/debug/ipmap
 else
   sudo setcap cap_net_raw,cap_net_admin=eip target/release/ipmap
--- src-web/src/routes/+page.svelte
+++ src-web/src/routes/+page.svelte
@@ -5,13 +5,23 @@
   import "ol/ol.css";
   import "../tailwind.css";
 
+  import Feature from "ol/Feature.js";
   import Map from "ol/Map.js";
-  import OSM from "ol/source/OSM.js";
-  import TileLayer from "ol/layer/Tile.js";
+  import Point from "ol/geom/Point.js";
   import View from "ol/View.js";
+  import { Circle as CircleStyle, Stroke, Style } from "ol/style.js";
+  import { OSM, Vector as VectorSource } from "ol/source.js";
+  import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
+  import { easeOut } from "ol/easing.js";
+  import { fromLonLat } from "ol/proj.js";
+  import { getVectorContext } from "ol/render.js";
+  import { unByKey } from "ol/Observable.js";
 
+  const source = new VectorSource();
+  const tile = new TileLayer({ source: new OSM() });
+  const vector = new VectorLayer({ source });
   let map: Map | null = null;
-  
+
   let locations: {
     ip: string;
     lat: number;
@@ -28,16 +38,17 @@
 
       connectionCount++;
       if (!uniqueIps.includes(payload.ip)) {
+        console.log("got a connection");
         uniqueIps.push(payload.ip);
+        addRandomFeature();
       }
     });
 
     map = new Map({
       target: "map",
       layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
+        tile,
+        vector,
       ],
       view: new View({
         center: [0, 0],
@@ -45,10 +56,62 @@
       }),
     });
   });
+
+  function addRandomFeature() {
+    const x = Math.random() * 360 - 180;
+    const y = Math.random() * 170 - 85;
+    const geom = new Point(fromLonLat([x, y]));
+
+    // source.addFeature(new Feature(geom));
+  }
+
+
+  
+  source.on('addfeature', function (e) {
+    flash(e.feature);
+  });
+
+  const flashDuration = 3000;
+  function flash(feature: Feature) {
+    const start = Date.now();
+    const flashGeom = feature.getGeometry().clone();
+    const listenerKey = tile.on('postrender', animate);
+
+    function animate(event) {
+      const frameState = event.frameState;
+      const elapsed = frameState.time - start;
+      if (elapsed >= flashDuration) {
+        unByKey(listenerKey);
+        return;
+      }
+      const vectorContext = getVectorContext(event);
+      const elapsedRatio = elapsed / flashDuration;
+      // radius will be 5 at start and 30 at end.
+      const radius = easeOut(elapsedRatio) * 25 + 5;
+      const opacity = easeOut(1 - elapsedRatio);
+
+      const style = new Style({
+        image: new CircleStyle({
+          radius: radius,
+          stroke: new Stroke({
+            color: 'rgba(255, 0, 0, ' + opacity + ')',
+            width: 0.25 + opacity,
+          }),
+        }),
+      });
+
+      vectorContext.setStyle(style);
+      vectorContext.drawGeometry(flashGeom);
+      // tell OpenLayers to continue postrender animation
+      if (map) {
+        map.render();
+      }
+    }
+  }
 </script>
 
-<h1>look at me</h1>
 <div class="w-screen h-screen flex">
+  <div class="w-30" />
   <div class="grow flex flex-col">
     {#if window.backendError}
       <div class="flow-root py-1 px-2 bg-red-600 text-gray-50 align-middle">
