aac6c482342f6baa645a73a5abdcee93e20ac0d8
grantshandy <granthandy@proton.me>
2024-05-15 12:38:17 -0600

    update icon and remove context menu

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import './app.css'
 import App from './App.svelte'
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 const app = new App({
   target: document.getElementById('app') as HTMLDivElement,
 })
