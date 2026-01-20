39428df6624f40c2b6455be73539e365c7d3c98e
grantshandy <granthandy@proton.me>
2026-01-20 12:58:40 -0700

    Clean up coordinate.rs

--- crates/ipgeo/src/coordinate.rs
+++ crates/ipgeo/src/coordinate.rs
@@ -1,5 +1,6 @@
-const LAT_MAX_ERROR: f32 = 0.00139;
-const LNG_MAX_ERROR: f32 = 0.00277;
+//! Types for working with and storing coordinates.
+
+use std::hash;
 
 const LAT_MAX: f32 = 90.0;
 const LNG_MAX: f32 = 180.0;
@@ -14,9 +15,10 @@ pub struct Coordinate {
 }
 
 impl Coordinate {
-    /// Returns true if the coordinates are equal within the packing error margin.
+    /// Returns true if the coordinates are equal within the precision of the database via `PackedCoordinate`.
     pub fn approx_eq(&self, other: &Self) -> bool {
-        (self.lat - other.lat).abs() < LAT_MAX_ERROR && (self.lng - other.lng).abs() < LNG_MAX_ERROR
+        pack_degree(self.lat, LAT_MAX) == pack_degree(other.lat, LAT_MAX)
+            && pack_degree(self.lng, LNG_MAX) == pack_degree(other.lng, LNG_MAX)
     }
 
     fn as_bytes(&self) -> u64 {
@@ -35,33 +37,34 @@ impl PartialEq for Coordinate {
 }
 impl Eq for Coordinate {}
 
-impl PartialOrd for Coordinate {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Coordinate {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.as_bytes().cmp(&other.as_bytes())
-    }
-}
-
-impl std::hash::Hash for Coordinate {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+impl hash::Hash for Coordinate {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
         self.as_bytes().hash(state);
     }
+}
+
+/// Converts from a packed u16 type to an f32 degree, given an absolute range (-abs_rng..=abs_rng).
+#[inline]
+const fn pack_degree(deg: f32, abs_rng: f32) -> u16 {
+    (((deg.clamp(-abs_rng, abs_rng) + abs_rng) / (abs_rng * 2.0)) * u16::MAX as f32).round() as u16
+}
+
+/// Converts from a packed u16 type to an f32 degree, given an absolute range (-abs_rng..=abs_rng).
+#[inline]
+const fn unpack_degree(deg_u: u16, abs_rng: f32) -> f32 {
+    (deg_u as f32 / u16::MAX as f32) * (abs_rng * 2.0) - abs_rng
 }
 
 /// A packed 32-bit representation of a global coordinate.
 ///
 /// Stores latitude and longitude as two 16-bit unsigned integers, allowing for efficient
-/// serialization and comparison. Conversion from/to `Coordinate` is lossy, but tests
+/// serialization and comparison. Conversion to/from `Coordinate` is lossy, but tests
 /// guarantee that it only loses at most a few hundred meters of precision.
+///
 /// See `coordinate::tests::packed_coordinate_max_error`.
 ///
 /// # Precision
-/// - Maximum latitude error: ~0.00139° (~ 155 meters)
+/// - Maximum latitude error: ~0.00139° (~155 meters)
 /// - Maximum longitude error: ~0.00277° (~308 meters at the equator; less at higher latitudes)
 ///
 #[derive(
@@ -88,31 +91,28 @@ pub(crate) struct PackedCoordinate {
 
 impl From<Coordinate> for PackedCoordinate {
     fn from(coord: Coordinate) -> Self {
-        let lat_norm = (coord.lat.clamp(-LAT_MAX, LAT_MAX) + LAT_MAX) / (LAT_MAX * 2.0);
-        let lng_norm = (coord.lng.clamp(-LNG_MAX, LNG_MAX) + LNG_MAX) / (LNG_MAX * 2.0);
-
-        let lat_u = (lat_norm * u16::MAX as f32).round() as u16;
-        let lng_u = (lng_norm * u16::MAX as f32).round() as u16;
-
-        PackedCoordinate { lat_u, lng_u }
+        PackedCoordinate {
+            lat_u: pack_degree(coord.lat, LAT_MAX),
+            lng_u: pack_degree(coord.lng, LNG_MAX),
+        }
     }
 }
 
 impl From<&PackedCoordinate> for Coordinate {
     fn from(packed: &PackedCoordinate) -> Self {
-        let lat_norm = packed.lat_u as f32 / u16::MAX as f32;
-        let lng_norm = packed.lng_u as f32 / u16::MAX as f32;
-
-        let lat = lat_norm * (LAT_MAX * 2.0) - LAT_MAX;
-        let lng = lng_norm * (LNG_MAX * 2.0) - LNG_MAX;
-
-        Coordinate { lat, lng }
+        Coordinate {
+            lat: unpack_degree(packed.lat_u, LAT_MAX),
+            lng: unpack_degree(packed.lng_u, LNG_MAX),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const LAT_MAX_ERROR: f32 = 0.00139;
+    const LNG_MAX_ERROR: f32 = 0.00277;
 
     const TEST_STEP: f32 = 0.025;
 
@@ -142,6 +142,7 @@ mod tests {
 
                 lng += TEST_STEP;
             }
+
             lng = -LNG_MAX;
             lat += TEST_STEP;
         }
